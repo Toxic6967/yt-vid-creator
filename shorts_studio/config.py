acc9bb2cb0961b986f6a459a94b6a7495c34b88b
Toxic6967 <Toxic.Sabi69@gmail.com>
2026-09-19 15:28:33 +0930

    Add cinematic story image-to-video workflow setting

--- shorts_studio/config.py
+++ shorts_studio/config.py
@@ -29,6 +29,10 @@ class Settings:
     comfyui_base_url: str = os.getenv("COMFYUI_BASE_URL", "http://127.0.0.1:8188")
     comfyui_image_checkpoint: str = os.getenv("COMFYUI_IMAGE_CHECKPOINT", "")
     comfyui_video_workflow: str = os.getenv("COMFYUI_VIDEO_WORKFLOW", str(WORKFLOW_DIR / "video_api.json"))
+    comfyui_story_video_workflow: str = os.getenv(
+        "COMFYUI_STORY_VIDEO_WORKFLOW",
+        str(WORKFLOW_DIR / "ltx_i2v_api.json"),
+    )
     max_source_chars: int = int(os.getenv("SHORTS_STUDIO_MAX_SOURCE_CHARS", "2400"))
     user_agent: str = os.getenv(
         "SHORTS_STUDIO_USER_AGENT",
