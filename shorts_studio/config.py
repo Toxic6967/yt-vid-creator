6ec7406f7ce9b27fa8287928946a3d82bbaf7fa2
Toxic6967 <Toxic.Sabi69@gmail.com>
2026-09-21 12:46:10 +0930

    Use a tighter default Shorts runtime

--- shorts_studio/config.py
+++ shorts_studio/config.py
@@ -54,7 +54,7 @@ class Settings:
     ollama_base_url: str = os.getenv("OLLAMA_BASE_URL", "http://127.0.0.1:11434")
     ollama_model: str = os.getenv("SHORTS_STUDIO_MODEL", "qwen3:8b")
     default_voice: str = os.getenv("SHORTS_STUDIO_VOICE", "auto-youthful-male")
-    default_target_seconds: int = int(os.getenv("SHORTS_STUDIO_TARGET_SECONDS", "65"))
+    default_target_seconds: int = int(os.getenv("SHORTS_STUDIO_TARGET_SECONDS", "58"))
     comfyui_base_url: str = os.getenv("COMFYUI_BASE_URL", "http://127.0.0.1:8188")
     comfyui_image_checkpoint: str = os.getenv("COMFYUI_IMAGE_CHECKPOINT", "")
     comfyui_video_workflow: str = os.getenv("COMFYUI_VIDEO_WORKFLOW", str(WORKFLOW_DIR / "video_api.json"))
